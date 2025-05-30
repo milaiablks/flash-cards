475d350af66d45c9948b2d665720cd4f6f0db939
milaiablks <camila.iablonski.santos@escola.pr.gov.br>
2025-05-30 13:12:24 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+CriaCartao(
+    'Fenômenos Estranhos',
+    'Existe um lugar no mundo onde ninguém consegue ficar em pé reto?',
+    'Na Gravitational Hill (ou Magnetic Hill), em vários países, carros e objetos parecem subir morros sozinhos. Na verdade, é uma ilusão de ótica — mas parece magia.'
+)
+
+CriaCartao(
+    'Espaço e Astronomia',
+    'Existe um planeta feito de diamante?',
+    'SIM. Ele se chama 55 Cancri e, fica a 40 anos-luz da Terra e acredita-se que até um terço do planeta seja puro diamante, por causa da pressão e composição do carbono. Pena que não dá pra pegar um pedaço e vender no Instagram.'
+)
+
+CriaCartao(
+    'Curiosidades da Natureza',
+    'Um fungo pode transformar formigas em zumbis?',
+    'Pode sim. O fungo se chama Ophiocordyceps unilateralis (nome de demônio né?) e controla o cérebro da formiga, fazendo ela subir numa planta, morder a folha e ficar lá até morrer. Aí o fungo cresce dentro dela. É o The Last of Us da natureza.'
+)
+
+CriaCartao(
+    'Lugares Misteriosos',
+    ' Existe uma floresta onde as árvores crescem tortas?',
+    'Sim! A Floresta Torta na Polônia tem mais de 400 pinheiros com troncos curvados em forma de "J". Até hoje ninguém sabe explicar por quê.'
+)
+
+CriaCartao(
+    'Curiosidades Científicas',
+    'Existe uma criatura marinha que pode viver pra sempre?',
+    'Sim! A água-viva Turritopsis dohrnii consegue reverter seu ciclo de vida, voltando à fase jovem. É considerada biologicamente imortal.'
+)
+
+CriaCartao(
+    'Simbolismo Misterioso',
+    'Qual é a teoria por trás do “olho que tudo vê”?',
+    'O olho que tudo vê, ou “Olho da Providência”, é um símbolo antigo que representa vigilância divina. Mas muitos associam ele aos Illuminati e ao controle secreto sobre a sociedade.'
+)
